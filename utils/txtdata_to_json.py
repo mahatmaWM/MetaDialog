5b019d73ffa3c98e9305b35df86d4a861719d769
SmallStom <70129412+SmallStom@users.noreply.github.com>
2021-02-04 16:32:11 +0800

    Add files via upload

---
+++ utils/txtdata_to_json.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import json
+import os
+import re
+
+
+def create_json_file(path, filename, jsonfilename):
+    """
+    parameters:path:
+    """
+    with open(path + filename, 'r', encoding='utf-8') as f:
+        datas = f.readlines()
+        dump_file = []
+        with open(path + jsonfilename + '.json', 'a', encoding='utf-8') as t:
+            for data in datas:
+                result = {}
+                text, domain, intent, label_context = data.strip().split('\t')
+                result['text'] = text
+                result['domain'] = domain
+                result['intent'] = intent
+                result['slots'] = {}
+                pattern = r'<(.+?)>(.+?)<'
+                res = re.findall(pattern, label_context)
+                for name in res:
+                    result['slots'][name[0]] = name[1]
+                dump_file.append(result)
+            json.dump(dump_file, t, ensure_ascii=False, indent=2)
+    return result
