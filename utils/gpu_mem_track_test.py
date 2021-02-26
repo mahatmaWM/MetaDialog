a005cd6b24d3846f808e43ab44fe4c8120048150
mahatma <wangming_0304@163.com>
2021-02-26 10:40:31 +0800

    update

--- utils/gpu_mem_track_test.py
+++ utils/gpu_mem_track_test.py
@@ -2,11 +2,33 @@ import torch
 import inspect
 
 from torchvision import models
-from gpu_mem_track import MemTracker  # 引用显存跟踪代码
+from gpu_mem_track import  MemTracker
 
-device = torch.device('cuda:0')
+cuda = 'cuda:1'
+device = torch.device(cuda)
 
-frame = inspect.currentframe()
-gpu_tracker = MemTracker(frame)      # 创建显存检测对象
+frame = inspect.currentframe()          # define a frame to track
+gpu_tracker = MemTracker(frame, verbose=False, device=1)         # define a GPU tracker
 
-gpu_tracker.track()                  # 开始检测
+gpu_tracker.track()                     # run function between the code line where uses GPU
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
+gpu_tracker.track()                     # run function between the code line where uses GPU
+
+dummy_tensor_1 = torch.randn(30, 3, 512, 512).float().to(device)  # 30*3*512*512*4/1000/1000 = 94.37M
+dummy_tensor_2 = torch.randn(40, 3, 512, 512).float().to(device)  # 40*3*512*512*4/1000/1000 = 125.82M
+dummy_tensor_3 = torch.randn(60, 3, 512, 512).float().to(device)  # 60*3*512*512*4/1000/1000 = 188.74M
+
+gpu_tracker.track()
+
+dummy_tensor_4 = torch.randn(120, 3, 512, 512).float().to(device)  # 120*3*512*512*4/1000/1000 = 377.48M
+dummy_tensor_5 = torch.randn(80, 3, 512, 512).float().to(device)  # 80*3*512*512*4/1000/1000 = 251.64M
+
+gpu_tracker.track()
+
+dummy_tensor_4 = dummy_tensor_4.cpu()
+dummy_tensor_2 = dummy_tensor_2.cpu()
+
+with torch.cuda.device(cuda):
+    torch.cuda.empty_cache()
+
+gpu_tracker.track()
