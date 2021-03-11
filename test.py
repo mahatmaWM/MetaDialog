d80ef4ffa355240cf97217524f79bac53b84d715
mahatma <wangming_0304@163.com>
2021-03-11 09:54:22 +0800

    test

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import json, codecs
 filename1 = '/Users/wangming/Desktop/test_pred.0.txt'
 y_true = list([])
 y_pred = list([])
+special_set = set(['[PAD]', '[PAD1]'])
 with codecs.open(filename1,'r', encoding='utf-8') as f1:
     datas = json.load(f1)
     for data in datas:
-        pred = data['pred']
-        label = data['label']
+        pred = data['pred'] if data['pred'][0] not in special_set else ['PAD']
+        label = data['label'] if data['label'][0] not in special_set else ['PAD']
         y_true.append(label)
         y_pred.append(pred)
 
